524547e3f620921f277188c22b17a7ea2b88125d
NirajRasal <nirajrasal17@gmail.com>
2020-07-05 20:00:43 +0530

    added score board

--- rock paper scissors/bunddle.js
+++ rock paper scissors/bunddle.js
@@ -1,27 +1,41 @@
 	let userinput;
 	let computerinput;
-
-	//document.getElementById('display').innerHTML=document.getElementById('rock').value;
-
-	//var rock=document.getElementById("rock");
+	let i=0;
+	let win=0;
+	let lose=0;
+	let draw=0;
+	//let username=document.getElementById('username').value;
+/*function usernamee()													not working
+{
+	username=document.getElementById("username").value;
+}
+console.log(username);*/
+for(i=0;i<5;i++)
+{
 	var options= ["rock","paper","scissors"];
 	function rock()
 	{
 		userinput='rock';
 		var computerinput = options[Math.floor(Math.random() * options.length)];//to get random array element
-		document.getElementById('display').innerHTML='user:'+userinput;
+		document.getElementById('display').innerHTML='you:'+userinput;
 		document.getElementById('display1').innerHTML='computer:'+computerinput;
 		if (computerinput=='rock')
 		{
-			document.getElementById('output').innerHTML="it is draw";    //not working
+			document.getElementById('output').innerHTML="it is draw";
+			draw++;  
+			document.getElementById('draw').innerHTML='draw='+draw;
 		}
 		else if (computerinput=='paper')
 		{
 			document.getElementById('output').innerHTML='you lose';
+			lose++;
+			document.getElementById('losser').innerHTML='lose='+lose;
 		}
 		else if(computerinput=='scissors')
 		{
 			document.getElementById('output').innerHTML='you win';
+			win++;
+			document.getElementById('winner').innerHTML='win='+win;
 		}
 		else{
 			document.getElementById('output').innerHTML='Something went wrong!!! Please try agian..';
@@ -31,19 +45,25 @@
 	{
 		var userinput='paper';
 		var computerinput = options[Math.floor(Math.random() * options.length)];//to get random array element
-		document.getElementById('display').innerHTML='user:'+userinput;
+		document.getElementById('display').innerHTML='you:'+userinput;
 		document.getElementById('display1').innerHTML='computer:'+computerinput;
 		if (computerinput=='rock')
 		{	
-			document.getElementById('output').innerHTML="you win";           // Adds to the document	not working
+			document.getElementById('output').innerHTML="you win";
+			win++;
+			document.getElementById('winner').innerHTML='win='+win;      
 		}
 		else if (computerinput=='paper')
 		{
 			document.getElementById('output').innerHTML='it is draw';
+			draw++;
+			document.getElementById('draw').innerHTML='draw='+draw;
 		}
 		else if(computerinput=='scissors')
 		{
 			document.getElementById('output').innerHTML='you lose';
+			lose++;
+			document.getElementById('losser').innerHTML='lose='+lose;
 		}
 		else{
 			document.getElementById('output').innerHTML='Something went wrong!!Please try agian..';
@@ -53,21 +73,31 @@
 	{
 		userinput='scissors';
 		var computerinput = options[Math.floor(Math.random() * options.length)];//to get random array element
-		document.getElementById('display').innerHTML='user:'+userinput;
+		document.getElementById('display').innerHTML='you:'+userinput;
 		document.getElementById('display1').innerHTML='computer:'+computerinput;
 		if (computerinput=='rock')
 		{
 			document.getElementById('output').innerHTML="you lose";
+			lose++;
+			document.getElementById('losser').innerHTML='lose='+lose;
 		}
 		else if (computerinput=='paper')
 		{
 			document.getElementById('output').innerHTML='you win';
+			win++;
+			document.getElementById('winner').innerHTML='win='+win;
 		}
 		else if(computerinput=='scissors')
 		{
 			document.getElementById('output').innerHTML='it is draw';
+			draw++;
+			document.getElementById('draw').innerHTML='draw='+draw;
 		}
 		else{
 			document.getElementById('output').innerHTML='Something went wrong!!Please try agian..';
 		}
 	}
+}
+//document.getElementById('rock').disabled=true;
+//document.getElementById('paper').disabled=true;
+//document.getElementById('scissors').disabled=true;
